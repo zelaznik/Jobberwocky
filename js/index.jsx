c9077a25311259e95ea3465c93ea6b5b9dd775f5
Steve Zelaznik <zelaznik@yahoo.com>
2016-03-26 01:07:27 -0400

    Restored the routing.

--- js/index.jsx
+++ js/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
-import App from './modules/App';
+import App from './modules/App.jsx';
 
 function placeHolder(name) {
     return class PlaceHolder extends React.Component {
-        render() {return (<h1>{name}</h1>)}
+        render() {return (<h1><center>{name}</center></h1>)}
     }
 }
 
@@ -15,6 +15,7 @@ let Dashboard = placeHolder('Dashboard');
 let Social = placeHolder('Social');
 let Charts = placeHolder('Charts');
 let Gallery = placeHolder('Gallery');
+let Error404 = placeHolder('404-Error');
 
 render((
     <Router history={ browserHistory } >
@@ -25,5 +26,6 @@ render((
             <Route path="/charts" component={Charts} />
             <Route path="/gallery" component={Gallery} />
         </Route>
+        <Route path="*" component={ Error404 } />
     </Router>
 ),document.getElementById('main'))
