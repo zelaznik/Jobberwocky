099727bf6c2140614ed23efb4c65d77edfcc3bff
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-06 14:14:08 -0400

    Dispatcher no longer allows undefined action types.

--- js/dispatcher/appDispatcher.jsx
+++ js/dispatcher/appDispatcher.jsx
@@ -1,3 +1,14 @@
-import { Dispatcher } from 'flux'
-const AppDispatcher = new Dispatcher();
+import { Dispatcher } from 'flux';
+import singleton from '../utils/singleton.jsx';
+
+class DispatcherType extends singleton(Dispatcher) {
+    dispatch(payload) {
+        if (payload.actionType == undefined)
+            throw new Error("Cannot dispatch undefined actionType.");
+        super.dispatch(...arguments);
+    }
+}
+
+var AppDispatcher = new DispatcherType();
+
 export default AppDispatcher;
