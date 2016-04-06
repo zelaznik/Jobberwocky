64f8eb7a1446e699da0ea479d044aca6890b3293
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-06 14:11:26 -0400

    Cleaned up unused Alert Actions.

--- js/actions/AlertActions.jsx
+++ js/actions/AlertActions.jsx
@@ -2,27 +2,6 @@ import AppDispatcher from '../dispatcher/AppDispatcher.jsx';
 import AlertConstants from '../constants/AlertConstants.jsx';
 
 const AlertActions = Object.freeze({
-    info(params) {
-        AppDispatcher.dispatch({
-            actionType: AlertConstants.INFO,
-            params: params
-        });
-    },
-
-    success(params) {
-        AppDispatcher.dispatch({
-            actionType: AlertConstants.SUCCESS,
-            params: params
-        });
-    },
-
-    warning(params) {
-        AppDispatcher.dispatch({
-            actionType: AlertConstants.WARNING,
-            params: params
-        });
-    },
-
     danger(params) {
         AppDispatcher.dispatch({
             actionType: AlertConstants.DANGER,
