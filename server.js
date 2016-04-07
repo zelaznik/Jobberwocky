a38164b5367a586a6317563ab01a17dcb07ffd79
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-06 23:11:36 -0400

    Serving assets from www for Heroku.

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ var compression = require('compression');
 
 var app = express();
 
+var PORT = process.env.PORT || 8080;
+
 // serve our static stuff like index.css
-app.use(express.static(__dirname));
+app.use(express.static("www"));
 
 // send all requests to index.html so browserHistory in React Router works
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'))
 });
 
-var PORT = process.env.PORT || 8080;
 app.listen(PORT, function() {
     console.log('Production Express server running at localhost:' + PORT);
 });
