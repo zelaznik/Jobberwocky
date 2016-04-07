307ac4cb1645ac9d74017cb70bd4b8bd87d57de6
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-07 03:49:34 -0400

    Added String formatter to utils library.

---
+++ js/utils/StringFormat.jsx
@@ -0,0 +1,22 @@
+var StringFormat = Object.freeze({
+    capitalize: function(orig) {
+        return orig[0].toUpperCase() + orig.slice(1).toLowerCase();
+    },
+
+    lowercase: function(orig) {
+        return orig.toLowerCase();
+    },
+
+    snake_to_label: function(orig) {
+        var words = orig.split(/_+/).map(StringFormat.capitalize);
+        return words.join(" ");
+    },
+
+    label_to_snake: function(orig) {
+        var words = orig.split(/\s+/).map(StringFormat.lowercase);
+        return words.join("_");
+    }
+
+});
+
+export default StringFormat;
