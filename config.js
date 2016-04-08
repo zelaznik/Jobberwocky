4d31f22c26d80cdc12e56233c240ebcb1da38ca1
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-08 14:31:41 -0400

    Got rid of the environment variables.  Not worth the hassle.

--- config.js
+++
@@ -1,6 +0,0 @@
-var env = process.env.NODE_ENV;
-console.log("NODE_ENV: " + env);
-if (env === undefined) {
-    throw new Error("You must specify an environment variable, NODE_ENV.");
-}
-module.exports = require('./environment/' + end + '.json');
--- environment/test.js
+++
@@ -1,3 +0,0 @@
-module.exports = Object.freeze({
-  API_ROOT_URL: 'http://api.jobberwocky.net'
-});
