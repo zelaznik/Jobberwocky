cec0577cb2afa81907b8e6110ddc96960cd3ab62
Steve Zelaznik <zelaznik@yahoo.com>
2016-07-05 14:36:00 -0400

    Cleaned up git directory.

--- js/stores/SessionStore.jsx
+++ js/stores/SessionStore.jsx
@@ -71,6 +71,7 @@ AppDispatcher.register((payload) => {
             break;
 
         case SessionConstants.OMNIAUTH_URL_PRELOAD:
+            console.log("Preload: " + payload.provider + ", " + payload.url);
             set_omniauth_url(payload.provider, payload.url);
             SessionStore.emitChange();
             break;
