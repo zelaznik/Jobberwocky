dd15b0838a343bfaccb16d0c3e8530eb21d8bd82
Steve Zelaznik <zelaznik@yahoo.com>
2016-07-09 18:43:19 -0400

    Added default avitar for user profile if image missing.

--- js/components/navbar/top/Top-DropDown-AccountSettings.jsx
+++ js/components/navbar/top/Top-DropDown-AccountSettings.jsx
@@ -27,7 +27,7 @@ var AccountSettingsDropDown = React.createClass({
         return (
             <li className={`dropdown ${this._clsOpen()} user hidden-xs`}>
                 <a data-toggle="dropdown" className="dropdown-toggle" href="#">
-                    { (!SessionStore.image()) ? '' : <img width="34" height="34" src={ SessionStore.image() } /> }
+                    <img width="34" height="34" src={ SessionStore.image() || 'assets/images/avatar-male.jpg' } />
                     { SessionStore.name() || SessionStore.email() }
                     <b className="caret" />
                 </a>
